999033ca119a6d2390c2eb8d3203d473a1f168f9
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2023-02-17 17:09:44 +0100

    update docstrings, general code documentation

--- CFPy/cfp/write_input.py
+++ CFPy/cfp/write_input.py
@@ -5,35 +5,55 @@ import os
 
 class write_input():
     """
+    Write the MODFLOW-CFP specific input files
+
     Dependencies: None
         
-    Input Variables:
-        
-        modelname: Name of the model (str)
-        data_strings: List of strings obtained from CFP classes
-        file_extensions: file extensions used by CFP (str)
-        
-    Input Files:
-    
-    Output: CFP input files (.coc, .crch, .cfp)
-    
+    Parameters
+    ----------        
+    modelname : name of the model; string
+    data_strings : list of strings obtained from CFP classes
+    file_extensions : list of strings representing file extensions used by CFP    
     """
     
-    def __init__(self, modelname, data_strings, file_extensions=['coc', 'crch', 'cfp']):
+    def __init__(
+        self,
+        modelname,
+        data_strings,
+        file_extensions=['coc', 'crch', 'cfp']
+        ):
         
         self.modelname = modelname
         self.data_strings = data_strings
         self.file_extensions = file_extensions
         
     def write_input(self):
+        """
+        Write the input files
 
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
+        # iterate over all files
         for i in range(len(self.file_extensions)):
+            # try to remove a previously existing file if it exists
             try:
                 os.remove(self.modelname + '.' + self.file_extensions[i])
+            # catch the possible error and do nothing in that case
             except FileNotFoundError:
                 pass
         
+        # iterate over all files
         for i in range(len(self.file_extensions)):
+            # open the file in writing mode
             with open (self.modelname + '.' + self.file_extensions[i], 'w') as file:
+                # iterate over the data strings
                 for string in self.data_strings[i]:
+                    # write the input to the file line by line
                     self.input = file.write(string + '\n')
